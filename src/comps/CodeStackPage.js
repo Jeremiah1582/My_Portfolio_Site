b697376c1b34f118aa7ec021e24363533a3c05ad
jeremiah1582 <jeremiah.1582@googlemail.com>
2022-07-08 15:51:59 +0200

    code stack page

--- src/comps/CodeStackPage.js
+++ src/comps/CodeStackPage.js
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React, {useContext, useState, useEffect}from 'react'
+import {codingLanguages} from "../context/datasets/codeStackDataset"
+import {Figure, Image, Card, ListGroup,ListGroupItem }from "react-bootstrap"
 function CodeStackPage() {
+  console.log(codingLanguages);
   return (
-    <div>Code Stack Page</div>
-  )
+    <div className="flexContainer" >
+      
+        {codingLanguages.map((item) => {
+          return (
+            <div style={{ padding: "10px" }}>
+              <Card style={{ width: "20rem", padding: "5px" }}>
+                <Card.Img variant="top" src={item.icon}></Card.Img>
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Text>{item.info.slice(0, 40).concat("...")}</Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>{item.type}</ListGroupItem>
+
+                  {item.paradigm ? (
+                    <ListGroupItem>Paradigms: {item.paradigm}</ListGroupItem>
+                  ) : null}
+                  {item.writtenIn ? (
+                    <ListGroupItem>WrittenIn: {item.writtenIn}</ListGroupItem>
+                  ) : null}
+                </ListGroup>
+                <Card.Body>
+                  <Card.Link href={item.documentation}>Documentation</Card.Link>
+                  {/* <Card.Link href="#">Another Link</Card.Link> */}
+                </Card.Body>
+              </Card>
+            </div>
+          );
+        })}
+    
+    
+    </div>
+  );
 }
 
 export default CodeStackPage
